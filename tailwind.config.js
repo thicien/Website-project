bafe39b2ad9042ecd5413705eca16a8679becc9d
mugisha thicien <mugishathicien04@gmail.com>
2025-11-12 20:37:53 +0200

    fix:fixing the fonts
    We used to implemet the fonts so that they meets the desired ones but there are some challanges we have to overcome them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+ content: [
+    "./index.html",
+    "./src//*.{html,js}",
+    "./src/sections/**/*.{html,js}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['"Outfit"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
-
