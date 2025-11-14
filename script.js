0b859bc3994b60ab22a1d9fd4c391697b289be51
mugisha thicien <mugishathicien04@gmail.com>
2025-11-14 12:00:49 +0200

    Fix:fixing of the responsiveness issue on tablets
    The responsiveness for all devices hass applied even the fonts matches those of provided figma

---
+++ script.js
@@ -0,0 +1,40 @@
+const loadComponent = (id, url, callback) => {
+        fetch(url)
+          .then((res) => {
+            if (!res.ok) throw new Error(`Failed to load ${url}`);
+            return res.text();
+          })
+          .then((html) => {
+            document.getElementById(id).innerHTML = html;
+            if (callback) callback();
+          })
+          .catch((err) => console.error(err));
+      };
+
+      window.addEventListener("DOMContentLoaded", () => {
+        loadComponent("navbar", "src/sections/navbar.html", setupMobileMenu);
+      });
+
+      function setupMobileMenu() {
+        const menuIcon = document.getElementById("mobileMenuIcon");
+        const menu = document.getElementById("mobileMenu");
+
+        if (!menuIcon || !menu) return;
+
+        let isMenuOpen = false;
+
+        menuIcon.addEventListener("click", () => {
+          isMenuOpen = !isMenuOpen;
+          menu.classList.toggle("hidden");
+
+          if (isMenuOpen) {
+            menuIcon.innerHTML = `
+        <img src="./images/closeButton.png" alt="Close menu icon" class="w-full h-full object-contain">
+      `;
+          } else {
+            menuIcon.innerHTML = `
+        <img src="./images/openButton.png" alt="Open menu icon" class="w-full h-full object-contain">
+      `;
+          }
+        });
+      }
