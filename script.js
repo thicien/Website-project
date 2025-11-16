131157708842b7b97904322352b7ee456639e5b7
jabo <arnoldjabo@gmail.com>
2025-11-16 04:43:47 +0200

    ref:removed-the-script-code-into-its-own-files
    we were given a review to include the codes inside the index html page into its own file for a 'DRY' principle enforcement

---
+++ script.js
@@ -0,0 +1,40 @@
+const loadComponent = (id, url, callback = setupMobileMenu) => {
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) throw new Error(`Failed to load ${url}`);
+      return res.text();
+    })
+    .then((html) => {
+      document.getElementById(id).innerHTML = html;
+      if (callback) callback();
+    })
+    .catch((err) => console.error(err));
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadComponent("hero-section", "src/sections/HeroSection.html");
+});
+
+function setupMobileMenu() {
+  const menuIcon = document.getElementById("mobileMenuIcon");
+  const menu = document.getElementById("mobileMenu");
+
+  if (!menuIcon || !menu) return;
+
+  let isMenuOpen = false;
+
+  menuIcon.addEventListener("click", () => {
+    isMenuOpen = !isMenuOpen;
+    menu.classList.toggle("hidden");
+
+    if (isMenuOpen) {
+      menuIcon.innerHTML = `
+        <img src="./images/closeButton.png" alt="Close menu icon" class="w-full h-full object-contain">
+      `;
+    } else {
+      menuIcon.innerHTML = `
+        <img src="./images/openButton.png" alt="Open menu icon" class="w-full h-full object-contain">
+      `;
+    }
+  });
+}
