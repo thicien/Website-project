b673bab6b43f0f1c0607a6cc1cd9e744cfe61ab9
mugisha thicien <mugishathicien04@gmail.com>
2025-11-20 15:34:14 +0200

    Fix:fixed the fonts and background color reviews as well as required

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const list = document.createElement("div");
   list.className =
-    "text-right space-y-1 text-black text-sm md:text-base font-semibold mr-32 pt-20 pb-20";
+    "text-right space-y-1 text-black text-[24px] md:text-base font-semibold mr-32 pt-20 pb-20";
 
   services.forEach(item => {
     const p = document.createElement("p");
