12a101af86551a23382cf7653bbaddba38dc97de
mugisha thicien <mugishathicien04@gmail.com>
2025-11-20 08:49:05 +0200

    Add: added JavaScript to the services now they are appearing dynamiclly  as lists to the page

--- script.js
+++ script.js
@@ -15,3 +15,55 @@ window.addEventListener("DOMContentLoaded", () => {
   loadComponent("services", "src/section/services.html");
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const services = [
+    "Ace Hotel",
+    "ARMEDANGELS",
+    "BERLIN DESIGN WEEK",
+    "Berliner Berg",
+    "BITE CLUB",
+    "Budweiser Budvar",
+    "ChungKing Noodles",
+    "Designpreis Brandenburg",
+    "Die Techniker",
+    "European Street Food Awards",
+    "Grundmann Dentistry",
+    "HORNBACH Werkstück",
+    "HORNBACH macht Schule",
+    "iF Design",
+    "Kopka",
+    "KLH Maßschuhe",
+    "Lode & Stijn",
+    "Luya",
+    "MEISSEN",
+    "Motel Beer & Coffee",
+    "PAPER & TEA",
+    "rocket & basil",
+    "Seeberger Gruppe",
+    "Shiori",
+    "SPOC Magazin",
+    "stocubo",
+    "STUR",
+    "World of Coffee",
+    "yamo"
+  ];
+
+  const container = document.getElementById("servicesContainer");
+
+  const list = document.createElement("div");
+  list.className =
+    "text-right space-y-1 text-black text-sm md:text-base font-semibold mr-32 pt-20 pb-20";
+
+  services.forEach(item => {
+    const p = document.createElement("p");
+    p.textContent = item;
+    list.appendChild(p);
+  });
+
+  container.appendChild(list);
+
+});
+
+
+
