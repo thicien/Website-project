10c26966371d2b056523344d1b9815140083b281
mugisha thicien <mugishathicien04@gmail.com>
2025-11-26 15:08:27 +0200

    Fix: fixed the unlickable and undirectable links with a yellow background color

--- script.js
+++ script.js
@@ -11,10 +11,6 @@ const loadComponent = (id, url, callback) => {
     .catch((err) => console.error(err));
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-  loadComponent("services", "src/section/services.html");
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   
   const services = [
@@ -53,18 +49,77 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const list = document.createElement("div");
   list.className =
-  "text-right text-black font-semibold text-[20px] md:text-[24px] lg:text-[24px] mr-20 lg:mr-32 pt-20 pb-20";
-
+    "text-right text-black font-semibold text-[20px] md:text-[24px] lg:text-[24px] mr-20 lg:mr-32 pt-20 pb-20";
 
   services.forEach(item => {
-    const p = document.createElement("p");
-    p.textContent = item;
-    list.appendChild(p);
+    const link = document.createElement("a");
+    link.textContent = item;
+
+    link.href = `./pages/${item.replace(/ /g, "-").toLowerCase()}.html`; 
+
+    link.className = "block cursor-pointer hover:text-green-600 transition-all";
+    list.appendChild(link);
   });
 
   container.appendChild(list);
 
 });
+
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   loadComponent("services", "src/section/services.html");
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+  
+//   const services = [
+//     "Ace Hotel",
+//     "ARMEDANGELS",
+//     "BERLIN DESIGN WEEK",
+//     "Berliner Berg",
+//     "BITE CLUB",
+//     "Budweiser Budvar",
+//     "ChungKing Noodles",
+//     "Designpreis Brandenburg",
+//     "Die Techniker",
+//     "European Street Food Awards",
+//     "Grundmann Dentistry",
+//     "HORNBACH Werkstück",
+//     "HORNBACH macht Schule",
+//     "iF Design",
+//     "Kopka",
+//     "KLH Maßschuhe",
+//     "Lode & Stijn",
+//     "Luya",
+//     "MEISSEN",
+//     "Motel Beer & Coffee",
+//     "PAPER & TEA",
+//     "rocket & basil",
+//     "Seeberger Gruppe",
+//     "Shiori",
+//     "SPOC Magazin",
+//     "stocubo",
+//     "STUR",
+//     "World of Coffee",
+//     "yamo"
+//   ];
+
+//   const container = document.getElementById("servicesContainer");
+
+//   const list = document.createElement("div");
+//   list.className =
+//   "text-right text-black font-semibold text-[20px] md:text-[24px] lg:text-[24px] mr-20 lg:mr-32 pt-20 pb-20";
+
+
+//   services.forEach(item => {
+//     const p = document.createElement("p");
+//     p.textContent = item;
+//     list.appendChild(p);
+//   });
+
+//   container.appendChild(list);
+
+// });
 
 
 
