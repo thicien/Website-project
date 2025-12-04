65254bbdf01490aebac545f252ef146587d95a68
jabo <arnoldjabo@gmail.com>
2025-12-04 11:02:19 +0200

    fix:erplace-the-img-tag-with-svg-tag
    with the normal HTML img tag it is hard to change the color of the image but with svg it is possible and easy to work with which will make it possible to change menu bar open and close icon have the same color as the overall content
    
    svg tag are the best fit for the kind of effect we wanted to be able to change the nav icons color in correspondant with the header text color
    
    now these will work as excpected if the header color changes to someting else this will make sure that the icons also have the same effects

--- script.js
+++ script.js
@@ -69,11 +69,52 @@ export function setupMobileMenu() {
 
     if (isMenuOpen) {
       menuIcon.innerHTML = `
-        <img src="/images/closeButton.png" alt="Close menu icon" class="w-full h-full object-contain">
+        <svg
+  viewBox="0 0 39 30"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-full h-full"
+>
+  <path
+    d="M1.10811 9.11842L0 19.1053L8.64865 28.3493L31.5135 30L39 19.0227L28 7L1.10811 9.11842Z"
+    class="fill-current"
+  />
+  <path
+    d="M21.072 24L17.52 19.008L17.112 18.624L12.624 12.6H15.24L18.48 17.04L18.888 17.376L23.688 24H21.072ZM12.408 24L17.112 17.664L18.336 19.248L14.952 24H12.408ZM19.056 18.6L17.808 17.064L20.952 12.6H23.472L19.056 18.6Z"
+    fill="white"
+  />
+</svg>
       `;
     } else {
       menuIcon.innerHTML = `
-        <img src="/images/openButton.png" alt="Open menu icon" class="w-full h-full object-contain">
+        <svg
+        viewBox="0 0 39 23"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-full h-full"
+      >
+        <path
+          d="M1.10811 2.11842L0 12.1053L8.64865 21.3493L31.5135 23L39 12.0227L28 0L1.10811 2.11842Z"
+          class="fill-current"
+        />
+        <line x1="10" y1="6" x2="26" y2="6" stroke="#EFECEA" stroke-width="2" />
+        <line
+          x1="10"
+          y1="11"
+          x2="26"
+          y2="11"
+          stroke="#EFECEA"
+          stroke-width="2"
+        />
+        <line
+          x1="10"
+          y1="16"
+          x2="26"
+          y2="16"
+          stroke="#EFECEA"
+          stroke-width="2"
+        />
+      </svg>
       `;
     }
   });
